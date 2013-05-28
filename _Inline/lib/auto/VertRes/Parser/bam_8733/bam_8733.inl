bd420e0f2490ec73f3d30c3522dc27d337a1df05
Carla Cummins <cc21@mib105307i.internal.sanger.ac.uk>
2013-05-28 15:57:47 +0100

    AddTagsToSeq
    Added function to extract tags, check if mapped, reverse complement if
    required and add to start of main sequences (DNA and quality)
    
    
    Former-commit-id: 1761a1d9dbc690eff90deab6d711c0a315185b9d

---
+++ _Inline/lib/auto/VertRes/Parser/bam_8733/bam_8733.inl
@@ -0,0 +1,22 @@
+md5 : 873301b0823257710f63a03a50f7c613
+name : VertRes::Parser::bam_8733
+version : ""
+language : C
+language_id : C
+installed : 0
+date_compiled : Tue May 28 10:30:12 2013
+inline_version : 0.53
+ILSM : %
+    module : Inline::C
+    suffix : bundle
+    type : compiled
+Config : %
+    apiversion : ?
+    archname : darwin-2level
+    cc : cc
+    ccflags : -fno-common -DPERL_DARWIN -fno-strict-aliasing -pipe -fstack-protector
+    ld : env MACOSX_DEPLOYMENT_TARGET=10.3 cc
+    osname : darwin
+    osvers : 12.3.0
+    so : dylib
+    version : 5.18.0
---
+++ lib/Bio/Tradis/CommandLine/AddTags.pm
@@ -0,0 +1,49 @@
+package Bio::Tradis::CommandLine::AddTags;
+
+# ABSTRACT: Add given tags to the start of the sequence
+
+=head1 SYNOPSIS
+
+Adds given tags to the start of the sequence
+
+=cut
+
+use Moose;
+use Getopt::Long qw(GetOptionsFromArray);
+use Cwd 'abs_path';
+use Bio::Tradis::AddTagsToSeq;
+
+has 'args'        => ( is => 'ro', isa => 'ArrayRef', required => 1 );
+has 'script_name' => ( is => 'ro', isa => 'Str',      required => 1 );
+has 'bamfile'     => ( is => 'rw', isa => 'Str',      required => 0 );
+has 'help'        => ( is => 'rw', isa => 'Bool',     required => 0 );
+
+sub BUILD {
+    my ($self) = @_;
+
+    my ( $bamfile, $help );
+
+    GetOptionsFromArray(
+        $self->args,
+        'b|bamfile=s' => \$bamfile,
+        'h|help'      => \$help
+    );
+
+    $self->bamfile( abs_path($bamfile) ) if ( defined($bamfile) );
+    $self->help($help) if ( defined($help) );
+
+}
+
+sub run {
+    my ($self) = @_;
+    if ( defined( $self->help ) ) {
+        print "Help here";
+    }
+
+    my $tagadd = Bio::Tradis::AddTagsToSeq->new(bamfile => $self->bamfile);
+    $tagadd->add_tags_to_seq;
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+1;
