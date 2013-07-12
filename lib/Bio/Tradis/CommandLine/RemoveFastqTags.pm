71f7e0e84bcaec7591363215ce9ea8ccfe0d1280
Carla Cummins <cc21@sanger.ac.uk>
2013-07-12 11:08:21 +0100

    Added tag mismatch option
    Users specify # mismatches allowed when matching tags

--- lib/Bio/Tradis/CommandLine/RemoveFastqTags.pm
+++ lib/Bio/Tradis/CommandLine/RemoveFastqTags.pm
@@ -17,24 +17,27 @@ has 'args'        => ( is => 'ro', isa => 'ArrayRef', required => 1 );
 has 'script_name' => ( is => 'ro', isa => 'Str',      required => 1 );
 has 'fastqfile'   => ( is => 'rw', isa => 'Str',      required => 0 );
 has 'tag'         => ( is => 'rw', isa => 'Str',      required => 0 );
-has 'help'        => ( is => 'rw', isa => 'Bool',     required => 0 );
-has 'outfile'     => ( is => 'rw', isa => 'Str',      required => 0 );
+has 'mismatch' => ( is => 'rw', isa => 'Int',  required => 0, default => 0 );
+has 'help'     => ( is => 'rw', isa => 'Bool', required => 0 );
+has 'outfile'  => ( is => 'rw', isa => 'Str',  required => 0 );
 
 sub BUILD {
     my ($self) = @_;
 
-    my ( $fastqfile, $tag, $outfile, $help );
+    my ( $fastqfile, $tag, $mismatch, $outfile, $help );
 
     GetOptionsFromArray(
         $self->args,
         'f|fastqfile=s' => \$fastqfile,
         't|tag=s'       => \$tag,
+        'm|mismatch=i'  => \$mismatch,
         'o|outfile=s'   => \$outfile,
         'h|help'        => \$help
     );
 
     $self->fastqfile( abs_path($fastqfile) ) if ( defined($fastqfile) );
     $self->tag($tag)                         if ( defined($tag) );
+    $self->mismatch($mismatch)               if ( defined($mismatch) );
     $self->outfile( abs_path($outfile) )     if ( defined($outfile) );
     $self->help($help)                       if ( defined($help) );
 
@@ -44,20 +47,22 @@ sub run {
     my ($self) = @_;
 
     if ( defined( $self->help ) ) {
-    #if ( scalar( @{ $self->args } ) == 0 ) {
-          $self->usage_text;
+
+        #if ( scalar( @{ $self->args } ) == 0 ) {
+        $self->usage_text;
     }
 
     my $tag_rm = Bio::Tradis::RemoveTags->new(
         fastqfile => $self->fastqfile,
         tag       => $self->tag,
+        mismatch  => $self->mismatch,
         outfile   => $self->outfile
     );
     $tag_rm->remove_tags;
 }
 
 sub usage_text {
-      print <<USAGE;
+    print <<USAGE;
 Removes transposon sequence and quality tags from the read strings
 
 Usage: remove_tags -f file.fastq [options]
@@ -65,10 +70,11 @@ Usage: remove_tags -f file.fastq [options]
 Options:
 -f  : fastq file with tradis tags
 -t	: tag to remove
+-m  : number of mismatches to allow when matching tag (optional. default = 0)
 -o  : output file name (optional. default: <file>.rmtag.fastq)
 
 USAGE
-      exit;
+    exit;
 }
 
 __PACKAGE__->meta->make_immutable;
--- lib/Bio/Tradis/RemoveTags.pm
+++ lib/Bio/Tradis/RemoveTags.pm
@@ -20,21 +20,23 @@ use Bio::Tradis::Parser::Fastq;
 
 has 'fastqfile' => ( is => 'rw', isa => 'Str', required => 1 );
 has 'tag'       => ( is => 'rw', isa => 'Str', required => 1 );
+has 'mismatch'  => ( is => 'rw', isa => 'Int', required => 0 );
 has 'outfile' => (
     is       => 'rw',
     isa      => 'Str',
     required => 0,
-    default => sub { 
-		my ($self) = @_;
-		my $o = $self->bamfile; 
-		$o =~ s/\.fastq/\.rmtag\.fastq/; 
-		return $o; 
-	}
+    default  => sub {
+        my ($self) = @_;
+        my $o = $self->bamfile;
+        $o =~ s/\.fastq/\.rmtag\.fastq/;
+        return $o;
+    }
 );
+
 sub remove_tags {
-    my ($self) = @_;
-    my $tag = uc( $self->tag );
-	my $outfile = $self->outfile;
+    my ($self)  = @_;
+    my $tag     = uc( $self->tag );
+    my $outfile = $self->outfile;
 
     #set up fastq parser
     my $filename = $self->fastqfile;
@@ -51,9 +53,18 @@ sub remove_tags {
         my $qual_string = $read[2];
 
         # remove the tag
-        my $l = length($tag);
-        if ( $seq_string =~ m/^$tag/ ) {
-            $seq_string =~ s/^$tag//;
+        my $rm = 0;
+        if ( $self->mismatch == 0 ) {
+            if ( $seq_string =~ m/^$tag/ ) { $rm = 1; }
+        }
+        else {
+            my $mm = $self->_tag_mismatch($seq_string);
+            if ( $mm <= $self->mismatch ) { $rm = 1; }
+        }
+
+        if ($rm) {
+            my $l = length($tag);
+            $seq_string  = substr( $seq_string,  $l );
             $qual_string = substr( $qual_string, $l );
         }
 
@@ -63,6 +74,21 @@ sub remove_tags {
     }
     close OUTFILE;
     return 1;
+}
+
+sub _tag_mismatch {
+    my ( $self, $seq_string ) = @_;
+    my $tag_len = length( $self->tag );
+
+    my @tag = split( "", $self->tag );
+    my @seq = split( "", substr( $seq_string, 0, $tag_len ) );
+    my $mismatches = 0;
+    foreach my $i ( 0 .. ( $tag_len - 1 ) ) {
+        if ( $tag[$i] ne $seq[$i] ) {
+            $mismatches++;
+        }
+    }
+    return $mismatches;
 }
 
 __PACKAGE__->meta->make_immutable;
