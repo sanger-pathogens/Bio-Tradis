34679cde6b76422a3f2f348a07dd54eaf4d4a640
Andrea Telatin <andrea@telatin.com>
2019-05-13 17:55:31 +0200

    Fixed XML

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -45,7 +45,7 @@ if [ "$TRAVIS" = 'true' ]; then
   echo "Using Travis's apt plugin"
 else
   sudo apt-get update -q
-  sudo apt-get install -y -q zlib1g-dev libgd-perl libgd-dev
+  sudo apt-get install -y -q zlib1g-dev libgd-perl libgd-dev libxml-libxml-perl 
 fi
 
 # Build all the things
